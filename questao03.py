2af4f6bb259f78362a0160f9772286be60bd9916
Raphael Pinho <73498424+raphapinho@users.noreply.github.com>
2023-10-05 22:33:47 -0300

    comentarios alterados

--- questao03.py
+++ questao03.py
@@ -9,7 +9,6 @@ scene = canvas(title="Representação  dos Vetores", width=800, height=600)
 arrow_A = arrow(pos=vector(0, 0, 0), axis=A, color=color.red, shaftwidth=0.1)
 arrow_B = arrow(pos=vector(0, 0, 0), axis=B, color=color.blue, shaftwidth=0.1)
 
-"""
 # b) Vetor resultante da soma vetorial
 result_soma = A + B
 arrow_sum = arrow(pos=vector(0, 0, 0), axis=result_soma, color=color.green, shaftwidth=0.1)
@@ -19,7 +18,7 @@ arrow_sum = arrow(pos=vector(0, 0, 0), axis=result_soma, color=color.green, shaf
 result_sub = A - B
 arrow_subtract = arrow(pos=vector(0, 0, 0), axis=result_sub, color=color.orange, shaftwidth=0.1)
 
-"""
+
 # d) Produto escalar e produto vetorial
 prod_escalar = dot(A, B)
 prod_vetorial = cross(A, B)
